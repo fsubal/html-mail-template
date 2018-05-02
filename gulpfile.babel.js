56d2ab9e00951f7dfe641646bd587f36251d7f32
subal f <subal@pixiv.com>
2018-05-02 18:39:13 +0900

    add scaffold

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ import { exec } from 'child_process';
 import json5 from 'json5';
 import glob from 'glob';
 import path from 'path';
+import minimist from 'minimist';
 
 gulp.task('default', ['compile-mail']);
 
@@ -37,6 +38,27 @@ gulp.task('compile-mail', () => {
       fs.unlinkSync(_tmp);
     }
   });
+});
+
+const createPug = name => `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+html
+  head
+    meta(content='ja', http-equiv='Content-Language')
+    meta(content='text/html; charset=utf-8', http-equiv='Content-Type')
+    meta(content='text/css', http-equiv='Content-Style-Type')
+    link(rel='stylesheet', type='text/css',href='../src/css/reset.css')
+    link(rel='stylesheet', type='text/css',href='../src/css/${name}.css')
+    title サンプルメールだよ
+  body
+`
+
+gulp.task('scaffold', () => {
+  const argv = minimist(process.argv.slice(2));
+  const { name } = argv;
+
+  fs.writeFileSync(`./src/${name}.pug`, createPug(name), { flag: 'wx' });
+  fs.writeFileSync(`./src/css/${name}.css`, '@charset "utf-8";', { flag: 'wx' });
+  fs.writeFileSync(`./src/model/${name}.json5`, '{}', { flag: 'wx' });
 });
 
 const execp = command => new Promise((resolve, reject) => (
